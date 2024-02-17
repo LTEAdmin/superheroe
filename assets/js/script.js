4f869584c26e12d2fa5832e76de12cec45011480
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-17 11:45:36 -0300

    Comenzando con el tema grafico de los poderes

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,27 +16,36 @@ function buscarSH(numSH) {
     success: function (data) {
       let conexiones = `${data.connections["group-affiliation"]} ${data.connections["relatives"]}`;
       let biography = `${data.biography["full-name"]} ${data.biography["alter-ego"]} ${data.biography["aliases"]} ${data.biography["place-of-birth"]}`;
-      let appearance = `${data.appearance["eye-color"]} ${data.appearance["gender"]} ${data.appearance["height"]} ${data.appearance["weight"]}`;
       let datoSH = `
         <div class="container">
             <h2>Super Heroe</h2>
             <div class="card my-3" style="max-width: 540px">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="${data.image.url}" class="img-fluid rounded-start" alt="..." id="imgSH"/>
+                        <img src="${
+                          data.image.url
+                        }" class="img-fluid rounded-start" alt="..." id="imgSH"/>
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
                         <h5 class="card-title" id="nameSH">${data.name}</h5>
                         <p class="card-text" id="alterEgoSH">${biography}</p>
                         <hr>
-                        <p class="card-text" id="descriptionSH">Publicado por: ${data.biography.publisher}</p>
+                        <p class="card-text" id="descriptionSH">Publicado por: ${
+                          data.biography.publisher
+                        }</p>
                         <hr>
-                        <p class="card-text " id="primeraAparicionSH">Primera aparicion: ${data.biography["first-appearance"]}</p>
+                        <p class="card-text " id="primeraAparicionSH">Primera aparicion: ${
+                          data.biography["first-appearance"]
+                        }</p>
                         <hr>
-                        <p class="card-text" id="alturaSH">Altura:  ${data.appearance.height}</p>
+                        <p class="card-text" id="alturaSH">Altura:  ${
+                          data.appearance.height
+                        }</p>
                         <hr>
-                        <p class="card-text" id="pesoSH">Peso: ${data.appearance.weight}</p>
+                        <p class="card-text" id="pesoSH">Peso: ${
+                          data.appearance.weight
+                        }</p>
                         <hr>
                         <p class="card-text" id="conexioneSH">Conexiones: ${conexiones}</p>
                         <p><small class="text-body-secondary">Last updated 3 mins ago</small</p>
@@ -44,10 +53,14 @@ function buscarSH(numSH) {
                     </div>
                 </div>
             </div>
-        </div> `;
+        </div> `     
+        
         $(".datosSH").append(datoSH);
-        grafico(data.powerstats);
-    },
+        debugger;
+        $("#numSH").val("");
+        grafico(data.powerstats, data.name);
+      },
+   
     error: function (error) {
       alert(` no se encontro el Super Heroe ${numSH}`);
     },
@@ -55,11 +68,35 @@ function buscarSH(numSH) {
   return this;
 }
 
-function grafico(data) {
+function grafico(datos, nombre) {    
     let ejes = [];
-    let grafico = `
-    <div class="Grafico container">
-        <canvas id="myChart" width="400" height="400"></canvas>
+    for (let atributo in datos) {
+        debugger;
+        ejes.push({ x: atributo, y: datos[atributo].value });
+    }
+    let datgrafico = {
+        title: {
+            text: ` Estadistica de poder para ${nombre}`,
+        },
+        data: [
+            {
+                type: "pie",
+                startAngle: 45,
+                showInLegend: "true",
+                legendText: "{ejes.name}",
+                indexlabel: "{ejes.name} ({ejes.y})",
+                yValuetFormatString: "#,##0 %",
+                dataPoints: ejes,
+            },
+        ],
+    };
+    let datosSH =
+    `<div class="Grafico container">
+        <canvas id="myChart" width="400" height="400"> ${grafico(
+        datos,
+        nombre
+    ).canvasjs()}</canvas>
         </div>`;
+  $(".datosSH").append(datosSH);
   return this;
 }
